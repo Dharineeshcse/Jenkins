6dfe85f765bf2fd8c3a9a9df14fa6bb0c90a0f71
Dharineesh <dharineesh22@gmail.com>
2026-07-22 17:54:54 +0530

    added venv in build.sh file

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 set -e
+
 echo "=================================="
 echo "Starting Build"
 echo "=================================="
 
 python --version
 
+echo "Creating Virtual Environment..."
+
+python -m venv .venv
+
+echo "Activating Virtual Environment..."
+
+source .venv/bin/activate
+
+echo "Installing Dependencies..."
+
+pip install --upgrade pip
 pip install -r app/requirements.txt
 
 echo "Dependencies Installed"
